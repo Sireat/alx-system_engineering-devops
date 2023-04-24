4dce8660b8748060e5f8d0ef947fa42e3fc75c57
Sireat <102405529+Sireat@users.noreply.github.com>
2023-04-24 21:53:35 +0300

    API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""Export data into the Json format"""
-
+"""Script that uses REST API"""
+import json
 import requests
 import sys
-import json
 
-if __name__ == '__main__':
-    endpoint = "https://jsonplaceholder.typicode.com/"
-    userId = sys.argv[1]
-    user = requests.get(endpoint + 'users/{}'.format(userId)).json()
-    todo = requests.get(endpoint + 'todos?userId={}'.format(userId)).json()
 
-    with open("{}.json".format(userId), 'w') as json_file:
-        json.dump({
-            userId:
-                [{
-                    "task": task.get("title"),
-                    "completed": task.get("completed"),
-                    "username": user.get("username")}
-                 for task in todo]},
-            json_file)
+def make_json(users=None, todos=None, u=None):
+    """Turns payloads into CSV format"""
+    all_list = []
+    with open(sys.argv[1] + ".json", "w") as f:
+        for i in todos:
+            all_list.append({"task": i.get("title"),
+                             "completed": i.get("completed"),
+                             "username": users[0].get("username")})
+        alljson = {str(u): all_list}
+        json.dump(alljson, f)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        args_id = {"id": sys.argv[1]}
+        users = requests.get("https://jsonplaceholder.typicode.com/users",
+                             params=args_id).json()
+        args_userid = {"userId": sys.argv[1]}
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                             params=args_userid).json()
+
+        make_json(users, todos, sys.argv[1])
